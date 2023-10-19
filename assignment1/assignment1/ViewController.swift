c2bb81a73220abb17644d87cad69340af29627ea
kim-seonwoo <sw204011@gmail.com>
2023-10-19 17:56:22 +0900

    storyboard delete to setup

--- assignment1/assignment1/ViewController.swift
+++ assignment1/assignment1/ViewController.swift
@@ -11,9 +11,11 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = .lightGray
         // Do any additional setup after loading the view.
     }
 
 
 }
 
+ 
