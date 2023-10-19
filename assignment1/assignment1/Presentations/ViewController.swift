965f5b7ee8b1443a8cc803da5d3bfd1b663bea12
kim-seonwoo <sw204011@gmail.com>
2023-10-19 20:30:10 +0900

    Set up file structure

---
+++ assignment1/assignment1/Presentations/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  assignment1
+//
+//  Created by Seonwoo Kim on 2023/10/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .lightGray
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+ 
